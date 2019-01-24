bc18d7535152fc2f4f591c9e822364ec13335404
benznest <panuwat.developer@gmail.com>
2019-01-24 18:12:33 +0700

    Add draggable feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -8,12 +10,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'SUDOKU Game',
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
+        primarySwatch: Colors.grey,
       ),
       home: MyHomePage(title: 'SUDOKU Game'),
     );
   }
 }
+
+const double RADIUS_CORNER = 12;
+const int VALUE_NONE = 0;
+
+const int COUNT_ROW_SUB_TABLE = 3;
+const int COUNT_COL_SUB_TABLE = 3;
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -24,178 +32,277 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class SudokuSubTable {
+  int indexRowInTable;
+  int indexColInTable;
+  List<List<int>> subTable;
+
+  SudokuSubTable({this.indexRowInTable, this.indexColInTable});
+
+  init() {
+    subTable = List();
+    for (int row = 1; row <= COUNT_ROW_SUB_TABLE; row++) {
+      List<int> list = List();
+      for (int col = 1; col <= COUNT_COL_SUB_TABLE; col++) {
+        list.add(0);
+      }
+      subTable.add(list);
+    }
+  }
+
+  int randomNumber() {
+    Random r = Random();
+    return r.nextInt(10);
+  }
+}
+
+class SudokuTable {
+  List<List<SudokuSubTable>> table;
+
+  init() {
+    table = List();
+    for (int row = 0; row < COUNT_ROW_SUB_TABLE; row++) {
+      List<SudokuSubTable> list = List();
+      for (int col = 0; col < COUNT_COL_SUB_TABLE; col++) {
+        SudokuSubTable subTable =
+            SudokuSubTable(indexRowInTable: row, indexColInTable: col);
+        subTable.init();
+        list.add(subTable);
+      }
+      table.add(list);
+    }
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
-
-  static const double RADIUS_CORNER = 12;
-  static const int CORNER_NONE = 0;
-  static const int CORNER_TOP_LEFT = 1;
-  static const int CORNER_TOP_RIGHT = 2;
-  static const int CORNER_BOTTOM_LEFT = 3;
-  static const int CORNER_BOTTOM_RIGHT = 4;
-
-
   /// Theme game
-  Color colorBorder = Colors.pink[600];
-  Color colorBackground = Colors.yellow[100];
-  Color colorBackground2 = Colors.yellow[200];
-
-  Color colorBackgroundSubTable1 = Colors.pink[100];
-  Color colorBackgroundSubTable2 = Colors.pink[200];
-  Color colorBackgroundNumberTab = Colors.pink[400];
+  ///
+  Color colorBorderTable = Colors.white;
+  Color colorBackgroundApp = Colors.blue[100];
+  Color colorBackgroundChannelEmpty1 = Color(0xffe3e3e3);
+  Color colorBackgroundChannelEmpty2 = Colors.white30;
+  Color colorBackgroundNumberTab = Colors.white;
   Color colorTextNumber = Colors.white;
-  Color colorBackgroundNumberBox = Colors.yellow[700];
+  Color colorBackgroundChannelValue = Colors.blue[700];
 
+  SudokuTable sudokuTable;
+
+  @override
+  void initState() {
+    initSudokuTable();
+    super.initState();
+  }
+
+  void initSudokuTable() {
+    sudokuTable = SudokuTable();
+    sudokuTable.init();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Container(constraints: BoxConstraints.expand(),
-            color: colorBackground,
+        body: Container(
+            constraints: BoxConstraints.expand(),
+            color: colorBackgroundApp,
             child: Column(children: <Widget>[
-              Expanded(child: Center(
-                  child: Column(mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Container(
-                          decoration: BoxDecoration(
-                              color: colorBorder,
-                              borderRadius: BorderRadius.all(Radius.circular(8))),
-                          padding: EdgeInsets.all(6),
-                          child: Column(
+//              Container(
+//                padding: EdgeInsets.only(top: 20),
+//                constraints: BoxConstraints.expand(height: 120),
+//                color: Colors.white,
+//                child: Center(
+//                    child: Text("",
+//                        style: TextStyle(
+//                          color: Colors.blue[700],
+//                            fontSize: 36,
+//                            fontWeight: FontWeight.bold))),
+//                margin: EdgeInsets.only(bottom: 16),
+//              ),
+              Expanded(
+                  child: Center(
+                      child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                          color: colorBorderTable,
+                          borderRadius: BorderRadius.all(Radius.circular(8))),
+                      padding: EdgeInsets.all(6),
+                      child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Row(
                               mainAxisSize: MainAxisSize.min,
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: <Widget>[
-                                Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    buildSubTable(CORNER_TOP_LEFT, colorBackgroundSubTable1),
-                                    buildSubTable(CORNER_NONE, colorBackgroundSubTable2),
-                                    buildSubTable(CORNER_TOP_RIGHT, colorBackgroundSubTable1),
-                                  ],
-                                ),
-                                Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    buildSubTable(CORNER_NONE, colorBackgroundSubTable2),
-                                    buildSubTable(CORNER_NONE, colorBackgroundSubTable1),
-                                    buildSubTable(CORNER_NONE, colorBackgroundSubTable2),
-                                  ],
-                                ),
-                                Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    buildSubTable(CORNER_BOTTOM_LEFT, colorBackgroundSubTable1),
-                                    buildSubTable(CORNER_NONE, colorBackgroundSubTable2),
-                                    buildSubTable(CORNER_BOTTOM_RIGHT, colorBackgroundSubTable1),
-                                  ],
-                                )
-                              ]),
-                        )
-                      ]
-                  )
-              )),
-              Container(padding: EdgeInsets.all(12), color: colorBackgroundNumberTab,
-                  child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                                buildSubTable(sudokuTable.table[0][0],
+                                    colorBackgroundChannelEmpty1),
+                                buildSubTable(sudokuTable.table[0][1],
+                                    colorBackgroundChannelEmpty2),
+                                buildSubTable(sudokuTable.table[0][2],
+                                    colorBackgroundChannelEmpty1),
+                              ],
+                            ),
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                buildSubTable(sudokuTable.table[1][0],
+                                    colorBackgroundChannelEmpty2),
+                                buildSubTable(sudokuTable.table[1][1],
+                                    colorBackgroundChannelEmpty1),
+                                buildSubTable(sudokuTable.table[1][2],
+                                    colorBackgroundChannelEmpty2),
+                              ],
+                            ),
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                buildSubTable(sudokuTable.table[2][0],
+                                    colorBackgroundChannelEmpty1),
+                                buildSubTable(sudokuTable.table[2][1],
+                                    colorBackgroundChannelEmpty2),
+                                buildSubTable(sudokuTable.table[2][2],
+                                    colorBackgroundChannelEmpty1),
+                              ],
+                            )
+                          ]),
+                    )
+                  ]))),
+              Container(
+                  padding: EdgeInsets.all(12),
+                  color: colorBackgroundNumberTab,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: buildNumberListTab()))
-            ])
-        )
+            ])));
+  }
+
+  Container buildSubTable(SudokuSubTable subTable, Color color) {
+    return Container(
+        padding: EdgeInsets.all(1),
+        child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+          Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: buildRowChannel(subTable, 0, color)),
+          Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: buildRowChannel(subTable, 1, color)),
+          Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: buildRowChannel(subTable, 2, color))
+        ]));
+  }
+
+  List<Widget> buildRowChannel(
+      SudokuSubTable subTable, int rowChannel, Color color) {
+    List<int> dataRowChanel = subTable.subTable[rowChannel];
+    List<Widget> listWidget = List();
+    for (int col = 0; col < 3; col++) {
+      Widget widget = buildChannel(rowChannel, dataRowChanel[col], color,
+          onNumberAccept: (data) {
+        setState(() {
+          sudokuTable.table[subTable.indexRowInTable][subTable.indexColInTable]
+              .subTable[rowChannel][col] = data;
+        });
+      }, onRemove: () {
+        setState(() {
+          sudokuTable.table[subTable.indexRowInTable][subTable.indexColInTable]
+              .subTable[rowChannel][col] = VALUE_NONE;
+        });
+      });
+      listWidget.add(widget);
+    }
+    return listWidget;
+  }
+
+  Widget buildChannel(int rowChannel, int value, Color color,
+      {Function(int) onNumberAccept, Function() onRemove}) {
+    if (value == 0) {
+      return DragTarget(builder: (BuildContext context, List<int> candidateData,
+          List<dynamic> rejectedData) {
+        return buildChannelEmpty();
+      }, onWillAccept: (data) {
+        return data >= 0 && data <= 9;
+      }, onAccept: (data) {
+        onNumberAccept(data);
+      });
+    } else {
+      return Draggable(
+        child: buildChannelValue(value),
+        feedback: Material(
+            type: MaterialType.transparency, child: buildChannelValue(value)),
+        childWhenDragging: buildChannelEmpty(),
+        onDragCompleted: () {
+          onRemove();
+        },
+        data: value,
+      );
+    }
+  }
+
+  Container buildChannelEmpty() {
+    return Container(
+      margin: EdgeInsets.all(1),
+      width: 40,
+      height: 40,
+      decoration: BoxDecoration(
+          color: colorBackgroundChannelEmpty1,
+          borderRadius: BorderRadius.all(Radius.circular(4))),
     );
   }
 
-  Container buildSubTable(int corner, Color color) {
-    int cornerRow1 = CORNER_NONE;
-    int cornerRow2 = CORNER_NONE;
-    int cornerRow3 = CORNER_NONE;
-
-    if (corner == CORNER_TOP_LEFT) {
-      cornerRow1 = CORNER_TOP_LEFT;
-    } else if (corner == CORNER_TOP_RIGHT) {
-      cornerRow1 = CORNER_TOP_RIGHT;
-    } else if (corner == CORNER_BOTTOM_LEFT) {
-      cornerRow3 = CORNER_BOTTOM_LEFT;
-    } else if (corner == CORNER_BOTTOM_RIGHT) {
-      cornerRow3 = CORNER_BOTTOM_RIGHT;
-    }
-
+  Widget buildChannelValue(int value) {
     return Container(
-        padding: EdgeInsets.all(1),
-        child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Row(mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: buildRowChannel(cornerRow1, color)),
-              Row(mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: buildRowChannel(cornerRow2, color)),
-              Row(mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: buildRowChannel(cornerRow3, color))
-            ]));
+      margin: EdgeInsets.all(1),
+      width: 40,
+      height: 40,
+      decoration: BoxDecoration(
+          color: colorBackgroundChannelValue,
+          borderRadius: BorderRadius.all(Radius.circular(4))),
+      child: Center(
+          child: Text(value.toString(),
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w900))),
+    );
   }
-
-  List<Widget> buildRowChannel(int corner, Color color) {
-    List<Widget> listWidget = List();
-    for (int col = 0; col < 3; col++) {
-      double tlRadius = corner == CORNER_TOP_LEFT && col == 0 ? RADIUS_CORNER : 0;
-      double trRadius = corner == CORNER_TOP_RIGHT && col == 2 ? RADIUS_CORNER : 0;
-      double blRadius = corner == CORNER_BOTTOM_LEFT && col == 0 ? RADIUS_CORNER : 0;
-      double brRadius = corner == CORNER_BOTTOM_RIGHT && col == 2 ? RADIUS_CORNER : 0;
-      Widget widget = buildChannel(
-          corner,
-          col,
-          tlRadius,
-          trRadius,
-          blRadius,
-          brRadius,
-          color);
-      listWidget.add(widget);
-    }
-    return listWidget;
-  }
-
-  Widget buildChannel(int row,
-      int col,
-      double tlRadius,
-      double trRadius,
-      double blRadius,
-      double brRadius,
-      Color color) =>
-      GestureDetector(onTap: () {},
-          child: Container(
-            margin: EdgeInsets.all(1),
-            width: 40,
-            height: 40,
-            decoration: BoxDecoration(
-                color: color,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(tlRadius),
-                    topRight: Radius.circular(trRadius),
-                    bottomLeft: Radius.circular(blRadius),
-                    bottomRight: Radius.circular(brRadius)
-                )),
-          ));
 
   List<Widget> buildNumberListTab() {
     List<Widget> listWidget = List();
-    for (int i = 0; i <= 9; i++) {
-      Widget widget = Container(
-          padding: EdgeInsets.symmetric(vertical: 4,horizontal: 8),
-          margin: EdgeInsets.symmetric(horizontal: 4),
-          decoration: BoxDecoration(color: colorBackgroundNumberBox,
-              borderRadius: BorderRadius.all(Radius.circular(8))),
-          child: Text("$i", style: TextStyle(
-              fontSize: 20,
-              color: colorTextNumber)));
+    for (int i = 1; i <= 9; i++) {
+      Widget widget = buildNumberBoxWithDraggable(i);
       listWidget.add(widget);
     }
     return listWidget;
   }
 
+  Widget buildNumberBoxWithDraggable(int i) {
+    return Draggable(
+      child: buildNumberBox(i),
+      feedback:
+          Material(type: MaterialType.transparency, child: buildNumberBox(i)),
+      data: i,
+    );
+  }
+
+  Container buildNumberBox(int i) {
+    return Container(
+        padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+        margin: EdgeInsets.symmetric(horizontal: 4),
+        decoration: BoxDecoration(
+            color: colorBackgroundChannelValue,
+            borderRadius: BorderRadius.all(Radius.circular(8))),
+        child: Text("$i",
+            style: TextStyle(
+                fontSize: 24,
+                color: colorTextNumber,
+                fontWeight: FontWeight.w900)));
+  }
 }
